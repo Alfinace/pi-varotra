e2f984d0e31dda6d05e0fca539c34cfa3f9fee53
Nantenaina Alfinace RAFANOMEZANTSOA <alfinace-akata@goavana.com>
2022-12-29 00:16:17 +0300

    edit

--- src/app/shared/components/modals/article-detail/article-detail.component.ts
+++ src/app/shared/components/modals/article-detail/article-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-article-detail',
@@ -6,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-detail.component.scss'],
 })
 export class ArticleDetailComponent implements OnInit {
+  @ViewChild('btnAddCart') btnAddCart: ElementRef;
+  constructor(
+    private modalController: ModalController) { }
 
-  constructor() { }
+  slideOpts = {
+    slidesPerView: 1,
+  };
+  ngOnInit() { }
 
-  ngOnInit() {}
+  public close() {
+    this.modalController.dismiss();
+  }
 
+  public logScrollStart() {
+    // this.btnAddCart.nativeElement.setAttribute('class', 'fixed-row btn-row mt');
+    // this.btnAddCart.nativeElement.style.opacity = '1';
+  }
+  public logScrolling(event: any) {
+    if (event.detail.scrollTop > 100) {
+      this.btnAddCart.nativeElement.setAttribute('class', 'fixed-row btn-row mt');
+    } else {
+      this.btnAddCart.nativeElement.setAttribute('class', 'btn-row mt');
+    }
+    this.btnAddCart.nativeElement.style.opacity = (event.detail.scrollTop / 500).toString();
+
+    // }
+    // this.btnAddCart.nativeElement.setAttribute('class', 'fixed-row btn-row mt');
+  }
+
+  public logScrollEnd() {
+    // this.btnAddCart.nativeElement.setAttribute('class', 'btn-row mt');
+    // this.btnAddCart.nativeElement.style.opacity = '1';
+  }
 }
--- src/app/shared/components/modals/article-detail/article-detail.module.ts
+++ src/app/shared/components/modals/article-detail/article-detail.module.ts
@@ -1,6 +1,8 @@
+import { IonicModule } from '@ionic/angular';
 import { ArticleDetailComponent } from './article-detail.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SwiperModule } from 'swiper/angular';
 
 
 
@@ -8,7 +10,9 @@ import { CommonModule } from '@angular/common';
   declarations: [ArticleDetailComponent],
   exports: [ArticleDetailComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule,
+    SwiperModule
   ]
 })
 export class ArticleDetailModule { }
