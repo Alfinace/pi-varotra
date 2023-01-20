d7cf0485f612a9c68c8164db429e4eb3640ddc78
Nantenaina Alfinace RAFANOMEZANTSOA <alfinace-akata@goavana.com>
2023-01-20 18:16:21 +0300

    edit env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: '',
-  prefix: 'PIVAROTRA_'
+  API_URL: 'https://api-service-pivarotra.onrender.com/api',
+  BACKEND_URL: 'https://api-service-pivarotra.onrender.com',
+  prefix: 'pi-varotra-prod_'
 };
