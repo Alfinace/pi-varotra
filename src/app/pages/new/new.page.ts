0edebc69658536ba9848c8e38aa010f6d789a63e
Nantenaina Alfinace RAFANOMEZANTSOA <alfinace-akata@goavana.com>
2023-02-05 22:34:55 +0300

    fix

--- src/app/pages/new/new.page.ts
+++ src/app/pages/new/new.page.ts
@@ -7,15 +7,29 @@ import { NewService } from 'src/app/services/new.service';
   templateUrl: './new.page.html',
   styleUrls: ['./new.page.scss'],
 })
-export class NewPage implements OnInit {
-  public news: New[] = [
-
-  ]
+export class NewPage {
+  public loading = true;
+  public news: New[] = []
+  public page: number = 0;
+  public pageSize: number = 10;
   constructor(private newService: NewService) { }
 
-  ngOnInit() {
+  ionViewWillEnter() {
     this.newService.getNews(0, 20).subscribe((news: any) => {
       this.news = news.rows;
+      this.loading = false;
+    })
+  }
+
+  loadData(event: any) {
+    this.page++;
+    this.newService.getNews(this.page, this.pageSize).subscribe((news: any) => {
+      this.news = news.rows;
+      this.loading = false;
+      event.target.complete();
+      if (this.news.length === news.count) {
+        event.target.disabled = true;
+      }
     })
   }
 
--- src/app/pages/store/store.page.ts
+++ src/app/pages/store/store.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { log } from 'console';
 import { Store } from 'src/app/models/store-model';
 import { StoreService } from 'src/app/services/store.service';
 
@@ -9,14 +10,37 @@ import { StoreService } from 'src/app/services/store.service';
 })
 export class StorePage implements OnInit {
   public stores: Store[] = [];
+  public page: number = 0;
+  public pageSize: number = 5;
+  count: any;
   constructor(private storeService: StoreService) { }
 
   ngOnInit() {
   }
 
   ionViewWillEnter() {
-    this.storeService.getStores(0, 10).toPromise().then(res => {
+    this.storeService.getStores(this.page, this.pageSize).toPromise().then(res => {
       this.stores = res.rows
+      this.count = res.count;
+    }).catch(err => {
+      console.log(err);
+
+    })
+  }
+
+  loadData(event: any) {
+    if (this.stores.length === this.count) {
+      event.target.disabled = true;
+      return;
+    }
+    this.page++;
+    this.storeService.getStores(this.page, this.pageSize).toPromise().then(res => {
+      this.stores.push(...res.rows);
+      this.count = res.count;
+      event.target.complete();
+      if (this.stores.length === res.count) {
+        event.target.disabled = true;
+      }
     }).catch(err => {
       console.log(err);
 
