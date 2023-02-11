f9542f4efa2481104363e0d5e7d71f0755a9f2f6
Nantenaina Alfinace RAFANOMEZANTSOA <alfinace-akata@goavana.com>
2023-02-11 17:20:38 +0300

    h

--- api/src/main.ts
+++ api/src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   const config = new DocumentBuilder().setTitle('API PI-vaotra').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('ui', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://api-service-pivarotra.onrender.com/api',
-  BACKEND_URL: 'https://api-service-pivarotra.onrender.com',
+  API_URL: 'http://151.80.136.113:7777/api',
+  BACKEND_URL: 'http://151.80.136.113:7777',
   prefix: 'pi-varotra-prod_'
 };
